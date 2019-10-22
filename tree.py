add67c02b036d1dbba908bb27a1afd4a5c238ea4
michaelstresing <mstresing@gmail.com>
2019-10-22 23:35:40 +0800

    fix insert and add BFS

--- tree.py
+++ tree.py
@@ -1,40 +1,69 @@
+from queue import Queue
+
 class BinaryTree:
 
     def __init__(self):
-        self._head = None
+        self._root = None
 
     def __repr__(self):
-        current = self._head
+        current = self._root
         result = []
         while current:
             left = current.left
             right = current.right
             result.append(f'{left}, {right}')
+            current = left
         return ' -> '.join(result)
 
-    def insert_node(self, value, node=None):
+    def insert_node(self, value):
         """
-        inserts a node into the binarytree
+        Public insert method
+        Allows input to be just value, checks root and passes parameters to
+        private method.
+        """
+        if self._root is None:
+            self._root = TreeNode(value)
+        else:
+            self._insert(self._root, value)
+
+    def _insert(self, node, value):
+        '''
+        Prive insert method, inserts a node into the binarytree
         :param value: the value of the node you're looking to insert
         :param node: the node, under which it will the new node will be inserted
-        """
-        if node is None:
-            self._head = TreeNode(value)
+        '''
 
-        elif value > node.value:
-            if node.right is None:
-                node.right = TreeNode(value)
-            else:
-                self.insert_node(value, node.left)
-
-        elif value < node.value:
+        if value > node.value:
             if node.left is None:
                 node.left = TreeNode(value)
             else:
-                self.insert_node(value, node.left)
+                self._insert(node.left, value)
 
-    def bredth_first_search(self, value):
-        ...
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._insert(node.right, value)
+
+    def breadth_first_search(self):
+        root = self._root
+        order = Queue()
+        nodes = []
+
+        order.put(root)
+
+        while order.qsize() > 0:
+
+            item = order.get()
+            nodes.append(item)
+
+            if item.left:
+                order.put(item.left)
+
+            if item.right:
+                order.put(item.right)
+
+        return nodes
 
     def depth_first_search(self, value):
         ...
@@ -42,21 +71,25 @@ class BinaryTree:
 
 class TreeNode:
 
-    def __init__(self, value=None, left=None, right=None):
+    def __init__(self, value=None):
         self.value = value
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
 
     def __repr__(self):
-        return f"Node:{self.val}"
+        return f"Node:{self.value}"
 
 
+t = BinaryTree()
+t.insert_node(1)
+t.insert_node(2)
+t.insert_node(3)
+t.insert_node(4)
+t.insert_node(5)
 
 
-
+bfs = t.breadth_first_search()
+print(bfs)
 
     # depth_first_search
     #     recursion
-    #
-    # breadth_first_search
-    #     queue
